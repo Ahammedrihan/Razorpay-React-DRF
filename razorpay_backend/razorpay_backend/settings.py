c102169c93ff62809753afe3958fb90f2deae81b
Ahammed <ahammedrihancm@gmail.com>
2023-12-12 14:55:40 +0530

    error frontend

--- razorpay_backend/razorpay_backend/settings.py
+++ razorpay_backend/razorpay_backend/settings.py
@@ -43,9 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+         'corsheaders.middleware.CorsMiddleware', 
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,6 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+
+
+CORS_ALLOWED_ORIGINS = [
+  
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
